9013277ed51000a290fa63d5c1114b375828db59
Bryan-Gallegos <64101108+Bryan-Gallegos@users.noreply.github.com>
2021-07-24 18:41:15 -0500

    creacion de sesion como administrador - cambio de variables

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <stdio.h>
+#include <conio.h>
+
 using namespace std;
 
 int menuPrincipal();
-void respuesta();
 void inicioSesion();
 void registro();
 void mostrarMenu();
+void administrador();
+void adminMenu();
 
 int main(){
     int valor;
@@ -16,16 +19,19 @@ int main(){
         case 1:  //inicio de sesion
             inicioSesion();
             break;
-        case 2: //registro de usuario
+        case 2:
+            administrador();
+            break;
+        case 3: //registro de usuario
             registro();
             break;
-        case 3:  //salir del programa
+        case 4:  //salir del programa
             break;
         default: //opcion no valida
             cout<<"\t\t Opcion desconocida"<<endl;
             break;
     }
-    
+
     return 0;
 }
 
@@ -33,32 +39,43 @@ int menuPrincipal(){
     int op;
     cout<<"============ Reservaciones de Pasajes ============"<<endl;
     cout<<" \t\t1. Iniciar Sesion  "<<endl;
-    cout<<" \t\t2. Registrarse "<<endl;
-    cout<<" \t\t3. Salir "<<endl;
+    cout<<" \t\t2. Iniciar como Administrador"<<endl;
+    cout<<" \t\t3. Registrarse "<<endl;
+    cout<<" \t\t4. Salir "<<endl;
     cout<<"=================================================="<<endl;
     cout<<" Ingrese una opcion: ";
-    
+
 
     cin>>op;
     return op;
 }
 
 void inicioSesion(){
-    
+
     cout<<"============ Inicio de Sesión ============"<<endl;
     cout<<"\t\t Usuario: "<<endl;
     cin >> usuario;
     cout<<"\n \t\t Contraseña: "<<endl;
     cin >> contrasenia;
-    /* 
-        Código de comprobación del usuario   
+    /*
+        Código de comprobación del usuario
      */
     cout<<"============ Inicio Exitoso! ============"<<endl;
     mostrarMenu();
 }
 
+void administrador(){
+    cout<<"============ Opciones============"<<endl;
+    cout<<"\t\t Correo. "<<endl;
+    cin>>correo,
+    cout<<"\t\t Contraseña: "<<endl;
+    cin>>contrasenia;
+
+    adminMenu();
+}
+
 void registro(){
-    
+
     cout<<"============ Registro ============"<<endl;
     cout<<"\t\t Nombre: <<endl;"<<endl;
     cin >> nombre;
@@ -67,34 +84,37 @@ void registro(){
     cout<<"\t\t Usuario: "<<endl;
     cin >> usuario;
     cout<<"\t\t Contraseña"<<endl;
-    cin >> contrasenia
+    cin >> contrasenia;
 
     /*
         Código de registro de usuario.
     */
-   cout<<"============ Registro Exitoso ============"<<endl;
+    cout<<"============ Registro Exitoso ============"<<endl;
 }
 void mostrarMenu(){
     int opc;
     cout<<"============ Opciones ============"<<endl;
     cout<<"\t\t 1. Nueva Reservacion: "<<endl;
-    cout<<"\t\t 2. Editar Reservacion: "<<endl;
+    cout<<"\t\t 2. Mostrar Reservacion: "<<endl;
     cout<<"\t\t 3. Eliminar Reservacion: "<<endl;
     cout<<"\t\t 4. Salir "<<endl;
     cout<<"=================================================="<<endl;
     cout<<" Ingrese una opcion: ";
+    cin>>opc;
     string nom;
     string dia;
-    cin.ignore();
     switch (opc){
         case 1: //Añadir nueva reservación
             cout<<endl;
             cout<<"\tIngrese nombre de lugar destino:"; getline(cin, nom);
+            /*
+                codigo con deestino
+            */
             cout<<"\tIngrese dia de salida:"; cin>>dia;
             break;
         case 2: //Mostrar todas las reservaciones
             cout<<endl;
-            /* 
+            /*
                 mostrar todos las reservaciones que hay
             */
             cout<<endl;
@@ -118,4 +138,52 @@ void mostrarMenu(){
             break;
     }
 }
+void adminMenu(){
+    int opci;
+    cout<<"============ Opciones ============"<<endl;
+    cout<<"\t\t 1. Crear aerolinea"<<endl;
+    cout<<"\t\t 2. Eliminar aerolinea"<<endl;
+    cout<<"\t\t 4. Generar Reporte "<<endl;
+    cout<<"\t\t 5. Salir"<<endl;
+    cout<<"=================================================="<<endl;
+    cout<<" Ingrese una opcion: ";
+    cin>>opci;
 
+    switch (opci) {
+        case 1:
+            cout<<endl;
+            cout<<"Ingrese nombre de aerolinea..\t";cin>>nombre;
+            cout<<endl;
+            cout<<"Ingrese destino de la aerolinea..\t";cin>>destino;
+            cout<<endl;
+            cout<<"Presione cualquier tecla para volver al menu.."<< endl;
+            getch();
+            break;
+        case 2:
+            cout<<endl;
+            /*
+                Mostrar todas las aerolineas
+             */
+            cout<<endl;
+            cout<<"Ingresar nombre de aerolinea a eliminar..\t";cin>>aero;
+            cout<<endl;
+            cout<<"Presione cualquier tecla para volver al menu.."<< endl;
+            getch();
+            break;
+        case 3:
+            cout<<endl;
+            cout<<"============ Generar Reporte ============";
+            /*
+             codigo de generacion de reporte
+             */
+            cout<<"Presione cualquier tecla para volver al menu.."<< endl;
+            getch();
+            break;
+        case 4:
+            cout<<"============ Fin del programa ============"<<endl;
+            break;
+        default:  //opcion no valida
+            cout << "Opción desconocida!" << endl;
+            break;
+    }
+}
